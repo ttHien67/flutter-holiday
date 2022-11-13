634f1fd893740663e1ee896d33eff0f0b7e27ada
ttHien67 <tthanhhien2k1@gmail.com>
2022-11-13 20:29:48 +0700

    Edit ui cua login

--- lib/ui/auth/app_banner.dart
+++ lib/ui/auth/app_banner.dart
@@ -15,27 +15,9 @@ class AppBanner extends StatelessWidget {
         vertical: 8.0,
         horizontal: 94.0,
       ),
-      transform: Matrix4.rotationZ(-8 * pi / 180)..translate(-10.0),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.deepOrange.shade900,
-        boxShadow: const [
-          BoxShadow(
-            blurRadius: 8,
-            color: Colors.black26,
-            offset: Offset(0, 2),
-          )
-        ],
-      ),
-      child: Text(
-        'MyShop',
-        style: TextStyle(
-          color: Theme.of(context).textTheme.headline6?.color,
-          fontSize: 50,
-          fontFamily: 'Anton',
-          fontWeight: FontWeight.normal,
-        ),
-      ),
-    );
+      child: const Image(
+        image: AssetImage('assets/image/logo-travel.png'),
+        fit: BoxFit.cover,
+      ));
   }
 }
--- lib/ui/auth/auth_screen.dart
+++ lib/ui/auth/auth_screen.dart
@@ -16,15 +16,10 @@ class AuthScreen extends StatelessWidget {
       body: Stack(
         children: <Widget>[
           Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  const Color.fromRGBO(215, 117, 255, 1).withOpacity(0.5),
-                  const Color.fromRGBO(255, 188, 117, 1).withOpacity(0.9),
-                ],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                stops: const [0, 1],
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/image/nature.jpg"),
+                fit: BoxFit.cover,
               ),
             ),
           ),
